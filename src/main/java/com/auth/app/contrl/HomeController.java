6c4519de6f9281de09d8ea1ce52fa5bfb92f833d
Rakhi Jha <rk5618985@gmail.com>
2023-10-21 23:54:16 +0530

    first time

---
+++ src/main/java/com/auth/app/contrl/HomeController.java
@@ -0,0 +1,22 @@
+package com.auth.app.contrl;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/signup")
+    public String signup(Model model){
+        model.addAttribute("title","");
+        return "signup";
+    }
+
+    @RequestMapping("/login")
+    public String login(Model model){
+        model.addAttribute("title","login page");
+        return "login";
+    }
+}
